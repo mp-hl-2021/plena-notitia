3dbca8db697b1c249eff4777a86b12df562898d3
Maria Malysheva <mn.malysheva@mail.ru>
2021-03-15 17:54:33 +0300

    Remove separation to public/private

--- api/api.go
+++ api/api.go
@@ -13,16 +13,11 @@ func NewRouter() http.Handler {
 	router.HandleFunc("/signup", postSignup).Methods(http.MethodPost)
 	router.HandleFunc("/signin", postSignin).Methods(http.MethodPost)
 
-	router.HandleFunc("/public/{page_id}", getPublicPage).Methods(http.MethodGet)
-	router.HandleFunc("/public/{page_id}", putPublicPage).Methods(http.MethodPut)
-	router.HandleFunc("/public/{page_id}", deletePublicPage).Methods(http.MethodDelete)
+	router.HandleFunc("/page/{page_id}", getPage).Methods(http.MethodGet)
+	router.HandleFunc("/page/{page_id}", putPage).Methods(http.MethodPut)
+	router.HandleFunc("/page/{page_id}", deletePage).Methods(http.MethodDelete)
 
-	router.HandleFunc("/private/{page_id}", getPrivatePage).Methods(http.MethodGet)
-	router.HandleFunc("/private/{page_id}", putPrivatePage).Methods(http.MethodPut)
-	router.HandleFunc("/private/{page_id}", deletePrivatePage).Methods(http.MethodDelete)
-
-	router.HandleFunc("/public/{page_id}/move", postMovePublicPage).Methods(http.MethodPost)
-	router.HandleFunc("/private/{page_id}/move", postMovePrivatePage).Methods(http.MethodPost)
+	router.HandleFunc("/page/{page_id}/move", postMovePage).Methods(http.MethodPost)
 
 	return router
 }
@@ -54,7 +49,7 @@ type getPageResponseModel struct {
 }
 
 // getPage handles request for page info.
-func getPage(w http.ResponseWriter, r *http.Request, isPublic bool) {
+func getPage(w http.ResponseWriter, r *http.Request) {
 	m := getPageResponseModel{Title: "Awesome nails", Subtitle: "", Links: []link{link{Name: "vk", Url: "https://vk.com/id0"}}}
 	if err := json.NewEncoder(w).Encode(m); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -62,44 +57,21 @@ func getPage(w http.ResponseWriter, r *http.Request, isPublic bool) {
 	}
 }
 
-// getPublicPage handles request for public page info.
-func getPublicPage(w http.ResponseWriter, r *http.Request) {
-	getPage(w, r, true)
-}
-
-// getPrivatePage handles request for private page info.
-func getPrivatePage(w http.ResponseWriter, r *http.Request) {
-	getPage(w, r, false)
+type putPageRequestModel struct {
+	IsPublic bool   `json:"is-public"`
+	Title    string `json:"title"`
+	Subtitle string `json:"subtitle"`
+	Links    []link `json:"links"`
 }
 
 // putPage writes page info.
-func putPage(w http.ResponseWriter, r *http.Request, isPublic bool) {
+func putPage(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
-}
-
-// putPublicPage writes public page info.
-func putPublicPage(w http.ResponseWriter, r *http.Request) {
-	putPage(w, r, true)
-}
-
-// putPrivatePage writes private page info.
-func putPrivatePage(w http.ResponseWriter, r *http.Request) {
-	putPage(w, r, false)
 }
 
 // deletePage deletes page.
-func deletePage(w http.ResponseWriter, r *http.Request, isPublic bool) {
+func deletePage(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
-}
-
-// deletePublicPage deletes public page.
-func deletePublicPage(w http.ResponseWriter, r *http.Request) {
-	deletePage(w, r, true)
-}
-
-// deletePrivatePage deletes private page.
-func deletePrivatePage(w http.ResponseWriter, r *http.Request) {
-	deletePage(w, r, false)
 }
 
 type postMovePageRequestModel struct {
@@ -108,16 +80,6 @@ type postMovePageRequestModel struct {
 }
 
 // postMovePage moves page to new address.
-func postMovePage(w http.ResponseWriter, r *http.Request, isPublic bool) {
+func postMovePage(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
-}
-
-// postMovePublicPage moves public page to new address.
-func postMovePublicPage(w http.ResponseWriter, r *http.Request) {
-	postMovePage(w, r, true)
-}
-
-// postMovePrivatePage moves private page to new address.
-func postMovePrivatePage(w http.ResponseWriter, r *http.Request) {
-	postMovePage(w, r, false)
 }
